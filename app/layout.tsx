e52453da6ff5fd38e0651ea74a5ca8517544152a
HanaElhady <144938741+HanaElhady@users.noreply.github.com>
2025-03-12 00:39:20 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/navigation/Header";
-import Footer from "./components/navigation/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
